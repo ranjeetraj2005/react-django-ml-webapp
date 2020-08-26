ca8572d14ea067a121f1271c106580476671f72a
ranjeet <ranjeetraj2005@gmail.com>
2020-08-27 04:19:41 +0530

    add file

--- kisanapp/tests.py
+++ kisanapp/tests.py
@@ -1,36 +1,19 @@
-import datetime
+from django.contrib.auth.models import AnonymousUser, User
+from django.test import TestCase, RequestFactory
 
-from django.test import TestCase
-from django.utils import timezone
-
-from .models import Question
+from .views import index
 
 
-class QuestionModelTests(TestCase):
+class SimpleTest(TestCase):
+    def setUp(self):
+        # Every test needs access to the request factory.
+        self.factory = RequestFactory()
 
-    def test_was_published_recently_with_future_question(self):
-        """
-        was_published_recently() returns False for questions whose pub_date
-        is in the future.
-        """
-        time = timezone.now() + datetime.timedelta(days=30)
-        future_question = Question(pub_date=time)
-        self.assertIs(future_question.was_published_recently(), False)
+    def test_details(self):
+        # Create an instance of a GET request.
+        request = self.factory.get("/")
+        request.user = AnonymousUser()
 
-    def test_was_published_recently_with_old_question(self):
-        """
-        was_published_recently() returns False for questions whose pub_date
-        is older than 1 day.
-        """
-        time = timezone.now() - datetime.timedelta(days=1, seconds=1)
-        old_question = Question(pub_date=time)
-        self.assertIs(old_question.was_published_recently(), False)
-
-    def test_was_published_recently_with_recent_question(self):
-        """
-        was_published_recently() returns True for questions whose pub_date
-        is within the last day.
-        """
-        time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
-        recent_question = Question(pub_date=time)
-        self.assertIs(recent_question.was_published_recently(), True)
+        # Test my_view() as if it were deployed at /customer/details
+        response = index(request)
+        self.assertEqual(response.status_code, 200)
--- kisanapp/views.py
+++ kisanapp/views.py
@@ -1,45 +1,19 @@
-from django.http import HttpResponseRedirect
-from django.shortcuts import get_object_or_404, render
-from django.urls import reverse
-from django.views import generic
+from django.shortcuts import render
+from django.http import HttpResponse
 
-from .models import Choice, Question
+from .models import Greeting
 
-
-class IndexView(generic.ListView):
-    template_name = 'kisanapp/index.html'
-    context_object_name = 'latest_question_list'
-
-    def get_queryset(self):
-        """Return the last five published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
+# Create your views here.
+def index(request):
+    # return HttpResponse('Hello from Python!')
+    return render(request, "index.html")
 
 
-class DetailView(generic.DetailView):
-    model = Question
-    template_name = 'kisanapp/detail.html'
+def db(request):
 
+    greeting = Greeting()
+    greeting.save()
 
-class ResultsView(generic.DetailView):
-    model = Question
-    template_name = 'kisanapp/results.html'
+    greetings = Greeting.objects.all()
 
-
-def vote(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    try:
-        selected_choice = question.choice_set.get(pk=request.POST['choice'])
-    except (KeyError, Choice.DoesNotExist):
-        # Redisplay the question voting form.
-        return render(request, 'kisanapp/detail.html', {
-            'question': question,
-            'error_message': "You didn't select a choice.",
-        })
-    else:
-        selected_choice.votes += 1
-        selected_choice.save()
-        # Always return an HttpResponseRedirect after successfully dealing
-        # with POST data. This prevents data from being posted twice if a
-        # user hits the Back button.
-        return HttpResponseRedirect(reverse('kisanapp:results', args=(question.id,)))
-
+    return render(request, "db.html", {"greetings": greetings})
